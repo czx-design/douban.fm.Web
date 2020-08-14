3f392e79f2b8c10f31aa19740d938b9a2f8bfb3d
czx-design <chenzhix1999@outlook.com>
2020-08-14 20:41:55 +0800

    准备开发，创建好项目了

---
+++ src/test/java/fm/douban/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package fm.douban.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
